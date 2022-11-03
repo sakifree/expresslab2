999a5e0020c1e6275902f1e0cf21ba541a72574f
Sakinah Freeman <freemansakinah@gmail.com>
2022-11-02 22:56:22 -0400

    Magic 8 Ball application created.

--- server.js
+++ server.js
@@ -14,10 +14,35 @@ app.get("/greeting/:name", (request, response) => {
 })
 
 app.get("/tip/:total/:tipPercentage", (request, response) => {
-    response.send("Your tip is $" + (request.params.total * (request.params.tipPercentage / 100))
+    response.send("Your tip is $" + (request.params.total * (request.params.tipPercentage / 100)))
 })
 
+app.get("/magic/:question", (request, response) => {
+    const answers = [
+        "It is certain", 
+        "It is decidedly so", 
+        "Without a doubt", 
+        "Yes definitely",
+        "You may rely on it", 
+        "As I see it yes", 
+        "Most likely", 
+        "Outlook good",
+        "Yes", 
+        "Signs point to yes", 
+        "Reply hazy try again", 
+        "Ask again later",
+        "Better not tell you now", 
+        "Cannot predict now", 
+        "Concentrate and ask again",
+        "Don't count on it", 
+        "My reply is no", 
+        "My sources say no",
+        "Outlook not so good", 
+        "Very doubtful"
+    ]
 
+    response.send(`${request.params.question}? <h1>${answers[Math.floor(Math.random()*answers.length)]}</h1>`)
+})
 
 
 
