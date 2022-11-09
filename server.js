b88cd863d5a02b97776930ef2742b757148ca2ad
Sakinah Freeman <freemansakinah@gmail.com>
2022-11-08 23:28:26 -0500

    files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 ///////////////////////////////////
 // ROUTES 
